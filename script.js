1926c736d24e09f2e32afd072aac21ca45aeb62f
luiz <luizvilela456@gmail.com>
2026-08-18 12:20:19 -0300

    efeitoscss

--- script.js
+++ script.js
@@ -42,27 +42,6 @@ function NextImg() {
 let lastScroll = 0;
 const header = document.querySelector('header');
 
-
-window.addEventListener('scroll', function () {
-    const currentScroll = window.pageYOffset;
-
-    if (currentScroll <= 0) {
-        header.classList.remove('header-hide', 'header-opacity');
-        return;
-    }
-
-    if (currentScroll > lastScroll) {
-        // Rolando para baixo
-        header.classList.add('header-hide');
-        header.classList.remove('header-opacity');
-    } else {
-        // Rolando para cima
-        header.classList.remove('header-hide');
-        header.classList.add('header-opacity');
-    }
-    lastScroll = currentScroll;
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     const menuHamburguer = document.getElementById('menu-hambuguer');
     const navBar = document.querySelector('.nav-bar');
@@ -101,12 +80,16 @@ document.addEventListener('DOMContentLoaded', function () {
     if (btnLerMais && prfExpandivel) {
         btnLerMais.addEventListener('click', function () {
             prfExpandivel.classList.toggle('expandido');
+            btnLerMais.classList.toggle('active');
             
-            // Alterar o texto do botão
-            if (prfExpandivel.classList.contains('expandido')) {
-                btnLerMais.textContent = 'Ler menos';
-            } else {
-                btnLerMais.textContent = 'Ler mais';
+            // Alterar o texto apenas do elemento .button-text
+            const buttonText = btnLerMais.querySelector('.button-text');
+            if (buttonText) {
+                if (prfExpandivel.classList.contains('expandido')) {
+                    buttonText.textContent = 'Ler menos';
+                } else {
+                    buttonText.textContent = 'Ler mais';
+                }
             }
         });
     }
